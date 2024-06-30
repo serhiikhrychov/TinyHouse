6112b36abc15df95a2154f38731e59be754c9ca2
Serhii <sergeyhall@gmail.com>
2024-06-30 13:11:20 +0200

    update eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World123!');
+const one: number = 1;
+
+console.log(one);
+
+app.get("/", (req, res) => {
+  res.send("Hello World123!");
 });
 
 app.listen(port);
 
 console.log(`[app]: http://localhost:${port}`);
-
