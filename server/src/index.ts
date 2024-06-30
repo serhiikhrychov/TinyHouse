18513d6d97a34627391f169ac9686a1913348884
Serhii <sergeyhall@gmail.com>
2024-06-30 18:13:24 +0200

    update apollo code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer();
-server.applyMiddleware({ app, path: "/api" });
+const startServer = async () => {
+  const server = new ApolloServer({ schema });
+  await server.start();
+  server.applyMiddleware({ app, path: "/api" });
 
-app.listen(port);
+  app.listen(port, () => {
+    console.log(`[app]: http://localhost:${port}`);
+  });
+};
 
-console.log(`[app]: http://localhost:${port}`);
+startServer().catch((error) => {
+  console.error("Error starting the server:", error);
+});
